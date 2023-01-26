24531c7f761353f6af241b7821a241ac07b2441e
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2023-01-26 18:40:24 -0500

    Update Program.cs

--- LIS2RT/LIS2RT/Program.cs
+++ LIS2RT/LIS2RT/Program.cs
@@ -11,10 +11,10 @@ namespace LIS2RT
     {
         static async Task Main(string[] args)
         {
-            string APIKey = "Sv3w0P3EFRbWRuUTwZlXOTKQv";
-            string APISecret = "iy1BZFCeXYeXPYJHk3qhLmRjC3WAGN21r5psIu15dXqUWy44ay";
-            string AccessToken = "1503505828946579456-1L2jzXXVdaqAYYFJaVktwbeLlLEVGp";
-            string AccessSecret = "zeotY3l0USvueTmzjevuhLspdGJo1GVB0OauZUC3iVf6l";
+            string APIKey = "";
+            string APISecret = "";
+            string AccessToken = "";
+            string AccessSecret = "";
             var files = new DirectoryInfo(@"C:\Users\bohnt\Desktop\LIS2RT\LIS2RT\LIS2RT\bin\Debug\netcoreapp3.1\LifeIsStrange2").GetFiles();
             int index = new Random().Next(0, files.Length);
             Console.WriteLine(files[index].Name);
