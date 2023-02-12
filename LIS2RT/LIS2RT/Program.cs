6496de6af3b9265e6354c93b24a963c5f87a23ea
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2023-02-11 23:11:17 -0500

    Update Program.cs

--- LIS2RT/LIS2RT/Program.cs
+++ LIS2RT/LIS2RT/Program.cs
@@ -5,7 +5,7 @@ using Tweetinvi.Models;
 using System.Threading.Tasks;
 using Tweetinvi.Parameters;
 
-namespace LIS2RT
+namespace TwitterBot
 {
      class Program
     {
@@ -15,13 +15,13 @@ namespace LIS2RT
             string APISecret = "";
             string AccessToken = "";
             string AccessSecret = "";
-            var files = new DirectoryInfo(@"C:\Users\bohnt\Desktop\LIS2RT\LIS2RT\LIS2RT\bin\Debug\netcoreapp3.1\LifeIsStrange2").GetFiles(); 
+            var files = new DirectoryInfo(@"C:\Users\user\folder").GetFiles(); 
             int index = new Random().Next(0, files.Length);
             Console.WriteLine(files[index].Name);
             byte[] ImageBytes = File.ReadAllBytes(files[index].Name);
             TwitterClient UserClient = new TwitterClient(APIKey, APISecret, AccessToken, AccessSecret);
             IMedia ImageIMedia = await UserClient.Upload.UploadTweetImageAsync(ImageBytes);
-            ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(new PublishTweetParameters("#LifeIsStrange2, #LIS2") { Medias = { ImageIMedia } });
+            ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(new PublishTweetParameters("Enter Text Here") { Medias = { ImageIMedia } });
         }
     }
 }
