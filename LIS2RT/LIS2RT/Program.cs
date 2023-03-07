fbc67636b523906eedab7534bfdbbd3ee20271f8
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2023-03-06 21:43:14 -0500

    Update Program.cs

--- LIS2RT/LIS2RT/Program.cs
+++ LIS2RT/LIS2RT/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
-using System.Threading.Tasks;
 using Tweetinvi.Parameters;
 
-namespace TwitterBot
+namespace LIS2RT
 {
-     class Program
+    class Program
     {
         static async Task Main(string[] args)
         {
@@ -15,13 +15,26 @@ namespace TwitterBot
             string APISecret = "";
             string AccessToken = "";
             string AccessSecret = "";
-            var files = new DirectoryInfo(@"C:\Users\user\folder").GetFiles(); 
+
+            var files = new DirectoryInfo(@"C:\Users\userhere\Desktop\LIS2RT\LIS2RT\LIS2RT\bin\Debug\netcoreapp3.1\LifeIsStrange2").GetFiles();
             int index = new Random().Next(0, files.Length);
             Console.WriteLine(files[index].Name);
-            byte[] ImageBytes = File.ReadAllBytes(files[index].Name);
+
+            byte[] ImageBytes = File.ReadAllBytes(files[index].FullName);
+
             TwitterClient UserClient = new TwitterClient(APIKey, APISecret, AccessToken, AccessSecret);
-            IMedia ImageIMedia = await UserClient.Upload.UploadTweetImageAsync(ImageBytes);
-            ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(new PublishTweetParameters("Enter Text Here") { Medias = { ImageIMedia } });
+
+            try
+            {
+                IMedia ImageIMedia = await UserClient.Upload.UploadTweetImageAsync(ImageBytes);
+                string tweetText = "#LifeIsStrange2, #LIS2";
+                ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(new PublishTweetParameters(tweetText) { Medias = { ImageIMedia } });
+                Console.WriteLine("Tweet sent!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error uploading image and sending tweet: {ex.Message}");
+            }
         }
     }
 }
